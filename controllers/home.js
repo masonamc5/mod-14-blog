39b3cfff5ef749d4daab3a496d0100e9a91c1e27
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-21 21:40:39 -0600

    added home routes

---
+++ controllers/home.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const { Post } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+    res.render("homepage", { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/new-post", (req, res) => {
+  res.render("newpost");
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error destroying session:", err);
+      res.status(500).json({ message: "Server error" });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+module.exports = router;
