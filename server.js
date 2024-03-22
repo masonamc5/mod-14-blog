3f7bd90da092db307faf590739a44771407826aa
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-21 21:40:13 -0600

    added the server logic

--- server.js
+++ server.js
@@ -1,49 +1,50 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const path = require("path");
 
-const blogRoutes = require('./controllers/blogRoutes');
-const sequelize = require('./config/connection');
+const homeRoutes = require("./controllers/home");
+const userRoutes = require("./controllers/user");
+const dashboardRoutes = require("./controllers/dashboard");
+const sequelize = require("./config/connection");
 
 const app = express();
-const hbs = exphbs.create();
+const PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(
   session({
-    secret: 'your-secret-key',
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
   })
 );
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', blogRoutes);
+app.use("/", homeRoutes);
+app.use("/user", userRoutes);
+app.use("/dashboard", dashboardRoutes);
 
 app.use((req, res, next) => {
   res.locals.loggedIn = req.session.logged_in || false;
   next();
 });
 
-app.get('/', (req, res) => {
-  console.log('get logged in status');
+app.get("/", (req, res) => {
   if (req.session.logged_in) {
-    console.log('logged in!');
-    res.redirect('/api/products');
+    res.redirect("/dashboard");
   } else {
-    console.log('need to log in');
-    res.render('login', {loggedIn: req.session.logged_in});
+    res.render("login", { loggedIn: req.session.logged_in });
   }
 });
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 });
