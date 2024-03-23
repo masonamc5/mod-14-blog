25cd89ed620d85918d99e253b83d3abead87e6c6
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-23 17:49:51 -0600

    updated db-url

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
